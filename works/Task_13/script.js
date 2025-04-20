b23958da6309c0a06459571e2db899bdf084dcdd
uthokala <uttamredddy861@gmail.com>
2025-04-20 12:07:26 -0500

    Updated changes

---
+++ works/Task_13/script.js
@@ -0,0 +1,13 @@
+const works = {
+    "task1": { "title": "Project 1", "url": "works/task1/index.html" },
+    "task2": { "title": "Project 2", "url": "works/task2/index.html" },
+    "task3": { "title": "Project 3", "url": "works/task3/index.html" }
+    
+};
+
+function loadTask(taskKey) {
+    const frame = document.getElementById("content-frame");
+    if (works[taskKey]) {
+        frame.src = works[taskKey].url;
+    }
+}
